08ec56bbeb6c483d4ca122f2b370af4fb13c5c6a
Solomon sokoya <solomonsokoya@Solomons-MacBook-Pro.local>
2018-03-30 11:28:36 -0400

    Beginning of Css

--- src/app.js
+++ src/app.js
@@ -1,95 +1,83 @@
-const dice = [1, 2, 3, 4, 5, 6];
+const di = [1, 2, 3, 4, 5, 6];
 let value = [];
-
-
-
-$('button').click(whoWon);
-
-
-
-
-
-
-
+$('button').click(playGame);
 
 function roll(dice) {
   return dice[Math.floor(Math.random() * dice.length)];
 }
 
-function setDice(){
-
+function setDice() {
   const playerRoll = [];
-   for (let i = 1; i <= 3; i+=1){
+  for (let i = 1; i <= 3; i += 1) {
+    const rolling = roll(di);
+    playerRoll.push(rolling);
+  }
 
-    const rolling = roll(dice);
-    playerRoll.push(rolling)
+  return (playerRoll);
+}
 
-   }
+function playGame() {
+  const temp = setDice();
+  console.log(temp);
 
-  return(playerRoll);
+  if (temp[0] === temp[1] && temp[1] === temp[2]) {
+    value.push(1 + temp[0]);
+    $('.content').text(`Trip ${temp[0]}`);
+  }
+  else if (temp[0] === temp[1] || temp[1] === temp[2] || temp[0] === temp[2]) {
+    temp.sort();
+    if (temp[0] === temp[1]) {
+      value.push(temp[2]);
+    }
+    else if (temp[1] === temp[2]) {
+      value.push(temp[0]);
+    }
+    else {
+      value.push(temp[1]);
+    }
+  }
+  else {
+    temp.sort();
+    if (temp[0] === 1 && temp[1] === 2 && temp[2] === 3) {
+      value.push(0);
+    }
+    else if (temp[0] === 4 && temp[1] === 5 && temp[2] === 6) {
+      value.push(20);
+
+
+    }
+    else {
+      console.log('roll again');
+    }
+  }
+  console.log(value);
+
+  $('.playerOne').text(value[0]);
+  $('.playerTwo').text(value[1]);
+
+setTimeout(compareValue,900);
+back();
 
 }
 
+function compareValue(){
 
-
-
-
-
-
-function whoWon() {
-
-  const dice1 = setDice();
-
-
-
- console.log(dice1)
-
-  if(dice1[0] === dice1[1] && dice1[1] === dice1[2]) {
-
-      value.push(1 + dice1[0]);
-
-       }
-  else if(dice1[0] === dice1[1] || dice1[1] === dice1[2] || dice1[0] === dice1[2]){
-      dice1.sort();
-      if(dice1[0] === dice1[1]){
-
-       value.push(dice1[2]);
-
-        }
-
-      else if (dice1[1] === dice1[2]) {
-
-
-        value.push(dice1[0]);
-      }
-      else{
-
-
-
-        value.push(dice1[1]);
-      }
+  if (value.length === 2) {
+    if (value[0] > value[1]) {
+      $('.content').text(`P1 Has Won`);
     }
-
-  else {
-       dice1.sort();
-    if(dice1[0] === 1 && dice1[1] === 2 && dice1[2] === 3){
-
-      value.push(0);
-
+    else if ( value[0] < value[1]){
+      $('.content').text(`P2 Has Won`);
     }
-
-    else if ( dice1[0] === 4 && dice1[1] === 5 && dice1[2] === 6){
-
-      value.push(20);
-
-    }
-
-    else{
-
-      console.log('roll again')
+    else {
+      $('.content').text(`Roll Again`);
     }
   }
+}
 
-  $('.playerOne').text(value[0]);
-$('.playerTwo').text(value[1]);
+function back(){
+
+  if(value.length === 1) {
+     $('.content').text(`P2 Turn`);
+  }
 }
