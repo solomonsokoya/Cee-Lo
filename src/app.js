bb3af9e69efb47f6efdb07cf2c11751e8d6e3e1e
Solomon sokoya <solomonsokoya@Solomons-MacBook-Pro.local>
2018-03-31 11:46:44 -0400

    Working On Animation

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const di = [1, 2, 3, 4, 5, 6];
 let value = [];
 $('button').click(playGame);
+$('button').click(startAni);
 
 function roll(dice) {
   return dice[Math.floor(Math.random() * dice.length)];
@@ -16,9 +17,19 @@ function setDice() {
   return (playerRoll);
 }
 
+function diePics(it) {
+  if (it[0] === 4) {
+    $('#die1').css('background-image:', 'url("../images/die4.png")');
+
+  }
+
+}
+
 function playGame() {
   const temp = setDice();
   console.log(temp);
+
+  diePics(temp);
 
   if (temp[0] === temp[1] && temp[1] === temp[2]) {
     value.push(1 + temp[0]);
@@ -81,3 +92,34 @@ function back(){
      $('.content').text(`P2 Turn`);
   }
 }
+
+
+function startAni(){
+
+  $('.die1').addClass('animation');
+
+}
+// $.keyframe.define({
+
+//   name:'roll',
+
+//   from: {
+//     'transform': 'scale(.5) rotate(0deg)'
+//   },
+//   to: {
+//     'transform' : 'scale(1) rotate(3600deg)'
+//   }
+
+// });
+
+// $('#die1').playKeyframe({
+//     name: 'roll', // name of the keyframe you want to bind to the selected element
+//     duration: '10s', // [optional, default: 0, in ms] how long you want it to last in milliseconds
+//     timingFunction: 'linear', // [optional, default: ease] specifies the speed curve of the animation
+//     delay: '0s', //[optional, default: 0s]  how long you want to wait before the animation starts
+//     iterationCount: 'infinite', //[optional, default:1]  how many times you want the animation to repeat
+//     direction: 'normal', //[optional, default: 'normal']  which direction you want the frames to flow
+//     fillMode: 'forwards', //[optional, default: 'forward']  how to apply the styles outside the animation time, default value is forwards
+//     complete: function(){} //[optional] Function fired after the animation is complete. If repeat is infinite, the function will be fired every time the animation is restarted.
+// });
+
