754176b4fd1881688fc57aace0a6d5936f97369c
Solomon sokoya <solomonsokoya@Solomons-MacBook-Pro.local>
2018-04-01 14:47:47 -0400

    Animation Complete

--- src/app.js
+++ src/app.js
@@ -1,6 +1,35 @@
 const di = [1, 2, 3, 4, 5, 6];
 let value = [];
-$('button').click(makeDice).click(playGame);
+let x = 1;
+
+$('button').click(function(){
+  if(x === 1){
+    makeDice();
+    playGame();
+    x++;
+  }
+  else if( x === 2) {
+    for (let n = 1 ; n <= 3  ; n ++){
+    let $di = $(`.die${n}`);
+    $di.toggleClass(`die${n+10}`);
+  }
+  playGame();
+  x++;
+  $('.button').text('Play Again');
+
+  }
+  else if( x === 3){
+   location.reload(true);
+  }
+});
+
+function moveDiceToTheLeft() {
+  for (let i = 1; i <= 3; i += 1) {
+    let $div= $(`.die${i}`);
+
+    $div.addClass(`die${i + 10}`);
+  }
+}
 
 function roll(dice) {
   return dice[Math.floor(Math.random() * dice.length)];
@@ -18,7 +47,7 @@ function setDice() {
 
 function diePics(diceArray) {
   for ( let i = 0; i < 3; i+= 1) {
-    $(`#die${i+1}`).attr('src', `images/die${diceArray[i]}.png`);
+    $(`.die${i+1}`).attr('src', `images/die${diceArray[i]}.png`);
   }
 }
 
@@ -60,7 +89,7 @@ function playGame() {
   $('.playerOne').text(value[0]);
   $('.playerTwo').text(value[1]);
 
-setTimeout(compareValue,850);
+setTimeout(compareValue,5000);
 back();
 
 }
@@ -68,7 +97,7 @@ back();
 function makeDice(){
   for( let i = 1; i <=3; i+= 1){
     let $div= $(`.dice${i}`);
-    $div.append(`<img id = "die${i}" src ="images/die1.png">`);
+    $div.append(`<img class = "die${i}" src ="images/die1.png">`);
   }
 }
 function compareValue(){
@@ -81,6 +110,14 @@ function compareValue(){
     }
     else {
       $('.content').text(`Play Again`);
+    }
+  }
+  else if(value.length === 1){
+    if (value[0] === 20) {
+      $('.content').text(`P1 Has Won`);
+    }
+    else if(value[0] === 0){
+      $('.content').text(`P2 Has Won`);
     }
   }
 }
